220189956be021d16397cfd52b8cb75d7d3febaf
Makiko Vaughan <makikovaughan@Makikos-MacBook-Pro.local>
2018-11-09 11:54:03 -0500

    changes to database setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// Imports express into our app and sets it up for use
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Port setup
+var PORT = process.env.PORT || 8080;
+
+// Sets up our server to parse our request body for usage
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//For the puclic folder
+app.use(express.static(path.join(__dirname, "./public")));
+
+// Routes
+require('./routes/api-user.js')(app);
+require('./routes/htmlRoute.js')(app);
+
+const db = require('./models');
+
+// Syncs our database first
+db.sequelize.sync().then(function(){
+
+  // Starts our server on the predefined PORT
+  // Only starts if the db successfully syncs
+  app.listen(PORT, function(){
+    console.log(`App is now listening on PORT ${PORT}`)
+  });
+});
