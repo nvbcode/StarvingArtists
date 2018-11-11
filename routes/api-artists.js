7445f0ad0420c2978fdb6a2179c38a7dd0a0c49b
Makiko Vaughan <makikovaughan@Makikos-MBP.attlocal.net>
2018-11-11 12:06:15 -0500

    modified api-artist.js and addingReviews.js

--- routes/api-artists.js
+++ routes/api-artists.js
@@ -65,17 +65,11 @@ module.exports = function (app) {
 
         db.Artist.create(artist).then(function (rows) {
 
-            console.log("Rows", rows.dataValues.id);
-
-            //Calling the function in addingReview.js to add the review.
-            creatingReview(artistData.reviews, rows.dataValues.id, function(){
-                res.json({
-                    ArtistId: rows.id,
-                    Status: "Created"
-                });
-            
+            res.json({
+                ArtistId: rows.dataValues.id,
+                Status: "Created"
             });
-                
+
         }).catch(function (error) {
             res.json({ error: error });
         });
@@ -83,18 +77,20 @@ module.exports = function (app) {
     });
 
 
-    //Create a row in Artists table.
-    // app.post("/api/reviews", function (req, res) {
+    //Create a row in Review table.
+    app.post("/api/reviews", function (req, res) {
 
-    //     db.Review.create(req.body).then(function (rows) {
-    //         res.json({
-    //             Review: rows.id,
-    //             Status: "Created"
-    //         });
-    //     }).catch(function (error) {
-    //         res.json({ error: error });
-    //     });
+        console.log(req.body);
 
-    // });
+        //Calling the function in addingReview.js to add the review.
+        creatingReview(req.body.reviews, req.body.ArtistId, function(){
+            res.json({
+                ArtistId: req.body.ArtistId,
+                Status: "Review Created"
+            });
+        
+        });
+
+    });
 
 }
