a63a083e2f899e4b3daf1cfa47fa0b03bd6433fa
unknown <bhandari.neel@outlook.com>
2018-11-12 14:12:34 -0500

    Adjusted api-user for login authentication. Still needing to add JWT

--- routes/api-user.js
+++ routes/api-user.js
@@ -6,20 +6,39 @@ const path = require('path');
 module.exports = function (app) {
 
 
-    //Create a row for user.
-    app.post('/api/users', function (req, res) {
+  //Create a row for user.
+  app.post('/api/users', function (req, res) {
 
+    console.log(req.body);
+    // added a conditional for duplicate users 
+
+    db.User.findAll({
+      where: {
+        username: req.body.userName
+      }
+    }).then(userRes => {
+      if (userRes.length >=1) {
+        return res.status(422).json({
+          message: "Username exists"
+        });
+      } else {
         console.log(req.body);
-
-        db.User.create(req.body).then(function (rows) {
-            res.json({
-              User: rows.id,
-              Status: "Created"
-            });
-          }).catch(function (error) {
-            res.json({ error: error });
+        // changing req.body to account for username and password
+        db.User.create({
+          username: req.body.userName,
+          password: req.body.password
+        }).then(function (rows) {
+          res.json({
+            User: rows.id,
+            Status: "Created"
           });
+        }).catch(function (error) {
+          res.json({ error: error });
+        });
 
-    });
+      }
+    }).catch();
+
+  });
 
 }
