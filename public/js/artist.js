914ddaa7b253deae77b49b26b6253f2fb2dc4e44
jjeng08 <jjeng08@users.noreply.github.com>
2018-11-11 19:18:49 -0500

    Added additional functionalities and did a lot of documentation to help ease the front-to-back request process.

--- public/js/artist.js
+++ public/js/artist.js
@@ -1,172 +1,192 @@
 //THIS IS ALL TEST DATA: ONCE THE FRONT AND BACK ARE LINKED, EVERYTHING BETWEEN THIS AND [END: DELETE ALL] WILL BE REMOVED.
 
-	//NOTE: we could create our own validation system by including a column randomly generated, but unique alphanumeric sequences. We could
-	// then pass this forward along with each request and send it back with each post with no worries because the odds of anyone being able 
-	// to accurately recreate another sequence as simple as 5 numbers and 3 letters) (i.e. 10^5 * 24^3 possibilities) is very low. 
+//NOTE: in the text data, all users IDs are 3 digits. All event IDs are 2 digits.
 
-	//[REQUEST]: GET info from database by [id].
-
-	const testArtist = {
-		id: 42,
-		firstName: "Bob",
-		lastName: "Bobberson",
-		profilePic: "https://i.ytimg.com/vi/chQmIQcWcO0/hqdefault.jpg",
-		artType: "Musician",
-		specialties: [
-			"smooth jazz sax",
-			"sexy jazz sax",
-			"sweet jazz sax for the soul"
-		],
-		//[REQUEST]: linked to [reviews] table. No actual request should be needed.
-		reviews: [
-			{
-				rating: 5,
-				comment: "His jazz is smooth."
-
-			},
-			{
-				rating: 5,
-				comment: "His jazz is sexy."
-			},
-			{
-				rating: 1,
-				comment: "His jazz does nothing for my soul."
-			}
-		],
-		//[REQUEST]: linked to [events] table. No actual request should be needed.
-		applications: [
-			22
-		]
-	}
-
-	//[REQUEST]: GET list of events from [events] table based on [artist] type.
-	const eventsList = [
+//[REQUEST]: GET info from database by artist's [id]. See the function startRender();
+const testArtist = {
+	id: 427,
+	firstName: "Bob",
+	lastName: "Bobberson",
+	profilePic: "https://i.ytimg.com/vi/chQmIQcWcO0/hqdefault.jpg",
+	artType: "Musician",
+	specialties: [
+		"smooth jazz sax",
+		"sexy jazz sax",
+		"sweet jazz sax for the soul"
+	],
+	//[REQUEST]: linked to [reviews] table. No actual request should be needed.
+	reviews: [
 		{
-			id: 10,
-			eventName: "Fluffy's Birthday Party",
-			zipcode: "90210",
-			price: 500,
-			state: "AK",
-			artType: "Musician",
-			comments: "I want Fluffy's birthday party to be special. He loves Ray Charles, so I'm hoping for someone that plays like that.",
+			rating: 5,
+			comment: "His jazz is smooth."
+
 		},
 		{
-			id: 22,
-			eventName: "Bar Mitzvah Bash",
-			zipcode: "19405",
-			price: 15,
-			state: "NJ",
-			artType: "Musician",
-			comments: "We need something cheap.",
+			rating: 5,
+			comment: "His jazz is sexy."
 		},
 		{
-			id: 37,
-			eventName: "Birth of the Antichrist",
-			zipcode: "10036",
-			price: 666,
-			state: "NY",
-			artType: "Musician",
-			comments: "Praise required for the advent of armageddon",
-		},
-		{
-			id: 99,
-			eventName: "The Annexation of Poland",
-			zipcode: "99999",
-			price: 1500,
-			state: "WS",
-			artType: "Artist",
-			comments: "Need a painter to create a commemorative piece for this great day.",
+			rating: 1,
+			comment: "His jazz does nothing for my soul."
 		}
+	],
+	//[REQUEST]: linked to [events] table. No actual request should be needed.
+	//NOTE: all [applications] arrays will initially be empty. This 22 is just in here for testing purposes (i.e. does the button disappear if
+	//the event ID is already stored).
+	applications: [
+		22
 	]
-	//[END: DELETE ALL]
+}
+
+//[REQUEST]: GET list of events from [events] table based on [artist] type. See the event eventsRender.
+//NOTE: the array [applicants] may not need to be stored directly here, but each event does need to track which artists applied to it.
+//NOTE: all applicants will initially be empty. This 427 is just in here for testing purposes.
+const events = [
+	{
+		id: 10,
+		eventName: "Fluffy's Birthday Party",
+		zipcode: "90210",
+		price: 500,
+		state: "AK",
+		artType: "Musician",
+		comments: "I want Fluffy's birthday party to be special. He loves Ray Charles, so I'm hoping for someone that plays like that.",
+		creator: 234,		//Tied to the "GenericPerson" client profile.
+		applicants: []
+	},
+	{
+		id: 22,
+		eventName: "Bar Mitzvah Bash",
+		zipcode: "19405",
+		price: 15,
+		state: "NJ",
+		artType: "Musician",
+		comments: "We need something cheap.",
+		creator: 117, 		//Not actually tied to anything, but every event needs to be tied to a client profile.
+		applicants: [427]
+	},
+	{
+		id: 37,
+		eventName: "Birth of the Antichrist",
+		zipcode: "10036",
+		price: 666,
+		state: "NY",
+		artType: "Musician",
+		comments: "Praise required for the advent of armageddon",
+		creator: 962,	 	//Not actually tied to anything, but every event needs to be tied to a client profile.
+		applicants: []
+	},
+	{
+		id: 99,
+		eventName: "The Annexation of Poland",
+		zipcode: "99999",
+		price: 1500,
+		state: "WS",
+		artType: "Artist",
+		comments: "Need a painter to create a commemorative piece for this great day.",
+		creator: 484,		//Not actually tied to anything, but every event needs to be tied to a client profile.
+		applicants: []
+	}
+]
+//[END: DELETE ALL]
 
 $(function () {
 
-	//PROFILE GENERATION: this section sets up the page to populate with the profile information.
-	function startRender() {
-
-
-
-		$.ajax({
-			method: "GET",
-			url: "/api/artists/1"
-		}).then(function (response) {
-			const artist = response;
-
-			$('#banner').append(`Welcome, ${artist.first_name} ${artist.last_name}!`);
-			$('#picBox').append(`<img id=profilePic src=${artist.profile_pic}>`);
-
-			const specialties = testArtist.specialties;
-			for (let i = 0; i < specialties.length; i++) {
-				$('#specialtiesBox').append(`<div class="specialtyItem">${specialties[i]}</div>`)
-			}
-
-			const reviews = artist.reviews;
-			for (let i = 0; i < reviews.length; i++) {
-				$('#reviewRow').append(`
-			<div class="oneReview">
-				<div class="rating">${reviews[i].review_rate}</div>
-				<div class="comment">${reviews[i].review_body}</div>
-			</div>`);
-			}
-
-			eventsRender(eventsList);
-
-
-		}).catch(function (err) {
-			console.log("Error", err);
-		});
-
-	}
-
 	startRender();
 
-//CREATING THE EVENTS LIST: This is a generic function that makes a list of available events. Called in PROFILE GENERATION.
-function eventsRender() {
+	//ARTIST PROFILE GENERATION: this section sets up the page to populate with the profile information.
+	function startRender() {
 
-		// $.get('/api/events')
-		// 	.then(function (events){}
+		// $.get('/api/profile/427') -- i.e. getting data based on the unique identifier.
+		// 	.then(function (profile){}
 
 		//[REQUEST]: GET [profile]. In the actual code: 
 		// 1. the test stuff above will be removed,
 		// 2. the code below will go into the {} of the .then request above, and 
-		// 3. eventsList in the code below will be replaced with the [events] argument seen above. Or vice versa. It's all good.
+		// 3. [testArtist] in the code below will be replaced with the [profile] argument seen above. Or vice versa. It's all good.
+		// 4. May need to add a return command in to make data accessible to click functions. Or maybe just create a universal variable?
+		$('#banner').append(`Welcome, ${testArtist.firstName} ${testArtist.lastName}!`);
+		$('#picBox').append(`<img id=profilePic src=${testArtist.profilePic}>`);
 
+		const specialties = testArtist.specialties;
+		for (let i = 0; i < specialties.length; i++) {
+			$('#specialtiesBox').append(`<div class="specialtyItem">${specialties[i]}</div>`)
+		}
 
-	for (i = 0; i < eventsList.length; i++) {
-		const e = eventsList[i];
-		if (e.artType === testArtist.artType)
-
-
-		$('#eventsBox').append(`
-			<div class="oneEvent">
-				<div class="eventElement">Event: ${e.eventName}</div>
-				<div class="eventElement">Offer: ${e.price}</div>
-				<div class="eventElement">Zipcode: ${e.zipcode}</div>
-				<div class="eventElement">State: ${e.state}</div>
-				<div class="eventElement">Comments: ${e.comments}</div>
-				<button class="applyButton" id="${e.id}">Apply</button>
-				<div class="notice hide" id="${e.id}notice">Applied!</div>
-			</div>
-		`)
+		const reviews = testArtist.reviews;
+		for (let i = 0; i < specialties.length; i++) {
+			$('#reviewRow').append(`
+			<div class="oneReview">
+				<div class="rating">${reviews[i].rating}</div>
+				<div class="comment">${reviews[i].comment}</div>
+			</div>`)
+		}
+		eventsRender();
 	}
 
-	for (i = 0; i < eventsList.length; i++) {
-		const e = eventsList[i]
-		if (testArtist.applications.includes(e.id)) {
-			$(`#${e.id}notice`).removeClass("hide");
-			$(`#${e.id}`).addClass("hide");
+	//CREATING THE EVENTS LIST: This is a generic function that makes a list of available events. Called in PROFILE GENERATION.
+	function eventsRender() {
+
+		// $.get('/api/events')
+		// 	.then(function (events){}
+
+		//[REQUEST]: GET [events]. In the actual code: 
+		// 1. the test stuff above will be removed,
+		// 2. the code below will go into the {} of the .then request above, and 
+		// 3. the [events] variabls below should correspond to the [events] argument that will be passed in.
+		// 4. May need to add a return command in to make data accessible to click functions. Or maybe just create a universal variable?
+		for (i = 0; i < events.length; i++) {
+			const e = events[i];
+			if (e.artType === testArtist.artType) {
+				$('#eventsBox').append(`
+					<div class="oneEvent">
+						<div class="eventElement">Event: ${e.eventName}</div>
+						<div class="eventElement">Offer: ${e.price}</div>
+						<div class="eventElement">Zipcode: ${e.zipcode}</div>
+						<div class="eventElement">State: ${e.state}</div>
+						<div class="eventElement">Comments: ${e.comments}</div>
+						<button class="applyButton" id="${e.id}">Apply</button>
+						<div class="notice hide" id="${e.id}notice">Applied!</div>
+					</div>`)
+			}
+		}
+
+		for (i = 0; i < events.length; i++) {
+			const e = events[i]
+			if (testArtist.applications.includes(e.id)) {
+				$(`#${e.id}notice`).removeClass("hide");
+				$(`#${e.id}`).addClass("hide");
+			}
 		}
 	}
-}
-	
+
 	$("#eventsBox").on("click", ".applyButton", applyEvent);
 	function applyEvent(event) {
 		event.preventDefault();
 
 		const eventID = parseInt($(this).attr('id'));
-
 		testArtist.applications.push(eventID);
+		// $.put('/api/profile/427')
+		//[REQUEST]: PUT [eventID]. In the actual code: 
+		// 1. the test stuff above will be removed, but we somehow still need to reference the object [testArtist].
+		//		NOTE: may end up reconstructing the whole thing with a return or just create a global-scale variabe (i.e. where the test
+		//		information currently sits.)
+		//		Since we're only pushing a single new piece of information, a global variable would probably be easy.
+
+		const eventIDList = [];
+		for (let i = 0; i < events.length; i++) {
+			eventIDList.push(events[i].id);
+		}
+
+		const index = eventIDList.indexOf(eventID);
+		events[index].applicants.push(testArtist.id);
+
+		// $.put('/api/events/${eventID}')
+		//[REQUEST]: PUT [events]. In the actual code: 
+		// 1. the test stuff above will be removed, but we somehow still need to reference the object [events].
+		//		NOTE: may end up reconstructing the whole thing with a return or just create a global-scale variabe (i.e. where the test
+		//		information currently sits.)
+		//		Since we're only pushing, a global variable would probably be easy.
+
 		$(`#${eventID}notice`).removeClass("hide");
 		$(`#${eventID}`).addClass("hide");
 	}
--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,19 @@
+//THIS IS ALL TEST DATA: ONCE THE FRONT AND BACK ARE LINKED, EVERYTHING BETWEEN THIS AND [END: DELETE ALL] WILL BE REMOVED.
+
+//NOTE: in the text data, all users IDs are 3 digits. All event IDs are 2 digits.
+
+//[REQUEST]: GET info from database by [id].
+
 const testUser = {
 	id: 234,
 	username: "GenericPerson",
-	password: "funfunfun"
+	password: "funfunfun",
+	createdEvents: [
+		10
+	]
 }
 
-const createdEvents = [
+const eventsList = [
 	{
 		id: 10,
 		eventName: "Fluffy's Birthday Party",
@@ -13,69 +22,70 @@ const createdEvents = [
 		state: "AK",
 		artType: "Musician",
 		comments: "I want Fluffy's birthday party to be special. He loves Ray Charles, so I'm hoping for someone that plays like that.",
+		creator: 234,
+		applications: [
+			427,		//Bob Bobberson, the example from the artist section.
+			619			//another example to simulate having multiple applicants.
+		]
+	},
+	{
+		id: 22,
+		eventName: "Bar Mitzvah Bash",
+		zipcode: "19405",
+		price: 15,
+		state: "NJ",
+		artType: "Musician",
+		comments: "We need something cheap.",
+		creator: 117, 		//Not actually tied to anything, but every event needs to be tied to a client profile.
+		applicants: [427]
 	}
 ]
 
+//[END: DELETE ALL]
+
 $(function () {
 
-function renderEvents() {
-	$('#eventsBox').empty();
-	
-	for (let i = 0; i<createdEvents.length; i++) {
-		const c = createdEvents[i];
-		$('#eventsBox').append(`
-		<div class="oneEvent">
-			<div class="eventElement">Event: ${c.eventName}</div>
-			<div class="eventElement">Type: ${c.artType}</div>
-			<div class="eventElement">Offer: ${c.price}</div>
-			<div class="eventElement">Comments: ${c.comments}</div>
-		</div>`)
-	}
-}
-
-
-
-	function startRender(){
-
-		$.ajax({
-			method: "GET",
-			url: "/api/customers/2"
-		}).then(function (response) {
-
-			const customer = response;
-
-		$('#banner').append(`Welcome, ${customer.first_name} ${customer.last_name}!`);
-
-		customer.events.forEach(event => {
-
-			const eventType= $('<div>').addClass("event-type").text(`Event Type: ${event.event_type}`);
-			const venueName= $('<div>').addClass("venue").text(`Venue: ${event.venue_name}`);
-			const street= $('<div>').addClass("street").text(`street: ${event.street_address}`);
-			const city = $('<div>').addClass("city").text(`City: ${event.city}`);
-			const state = $('<div>').addClass("state").text(`State: ${event.state}`);
-			const budget = $('<div>').addClass("budget").text(`Budget: ${event.budget}`);
-			const additionalInfo = $('<div>').addClass("additional-info").text(`Additional Information: ${event.additional_info}`);
-
-			let hasBooking;
-
-			if (event.has_booking) {
-				hasBooking = $('<div>').addClass("budget").text(`Booking: Yes`);
-			}
-			else {
-				hasBooking = $('<div>').addClass("budget").text(`Booking: Yes`);
-			}
-			
-
-			$(".boxBanner").append(eventType).append(venueName).append(street).append(city).append(state);
-			$(".boxBanner").append(budget).append(additionalInfo).append(hasBooking).append("<hr>");
-
-		});
-
-		});
-
-	}
-
 	startRender();
+	function startRender() {
+		// $.get('/api/user/234') -- i.e. getting data based on the unique identifier.
+		// 	.then(function (user){}
+
+		//[REQUEST]: GET [user]. In the actual code: 
+		// 1. the test stuff above will be removed,
+		// 2. the code below will go into the {} of the .then request above, and 
+		// 3. [testUser] in the code below will be replaced with the [user] argument seen above. Or vice versa. It's all good.
+		// 4. May need to add a return command in to make data accessible to click functions. Or maybe just create a universal variable?
+		$('#banner').append(`Welcome, ${testUser.username}!`);
+		renderEvents();
+	}
+
+	//CREATING THE EVENTS LIST: This is a generic function that makes a list of available events. Called in the startRender function 
+	//and the createEvent click function.
+	function renderEvents() {
+		// $.get('/api/events/234')
+		// 	.then(function (events){}
+
+		//[REQUEST]: GET [events]. In the actual code: 
+		// 1. the test stuff above will be removed,
+		// 2. the code below will go into the {} of the .then request above, and 
+		// 3. the [eventsList] variables below should correspond to the [events] argument that will be passed in.
+		// 4. May need to add a return command in to make data accessible to click functions. Or maybe just create a universal variable?
+		$('#eventsBox').empty();
+
+		for (let i = 0; i < eventsList.length; i++) {
+
+			const c = eventsList[i];
+			if (testUser.id === c.creator) {
+				$('#eventsBox').append(`
+					<div class="oneEvent">
+						<div class="eventElement">Event: ${c.eventName}</div>
+						<div class="eventElement">Type: ${c.artType}</div>
+						<div class="eventElement">Offer: ${c.price}</div>
+						<div class="eventElement">Comments: ${c.comments}</div>
+					</div>`);
+			}
+		}
+	}
 
 	$('#createEvent').on("click", showEventModal)
 	function showEventModal(event) {
@@ -86,13 +96,18 @@ function renderEvents() {
 	$("#createButton").on("click", createEvent)
 	function createEvent(event) {
 		event.preventDefault();
+		//VERY IMPORTANT! Currently, this event ID is being randomly generated for testing. In the actual program, this will not exist, 
+		//and mySQL will take care of it.
+		const fakeID = Math.floor(Math.random() * 100)
 
 		const newEvent = {
+			id: fakeID,
 			eventName: $('#eventName').val().trim(),
 			zipcode: parseInt($('#zipcode').val()),
 			price: parseInt($('#zipcode').val()),
 			state: $('#state').val(),
-			artist: $('#virtuoso').val(),
+			artType: $('#virtuoso').val(),
+			creator: testUser.id,
 			comments: $('#comments').val()
 		}
 
@@ -100,14 +115,27 @@ function renderEvents() {
 			$('#errorBox').addClass("show")
 			$('#errorBox').toggleClass("alt");
 		} else {
-			createdEvents.push(newEvent);
+			eventsList.push(newEvent);
+			// $.put('/api/events)
+			//[REQUEST]: PUT [newEvents]. In the actual code: 
+			// 1. the test stuff above will be removed, but we somehow still need to reference the object [testUser].
+			//		NOTE: may end up reconstructing the whole thing with a return or just create a global-scale variabe (i.e. where the test
+			//		information currently sits.)
+			//		Since we're only pushing, a global variable would probably be easy.
+
+			testUser.createdEvents.push(newEvent.id);
+			// $.put('/user)
+			//[REQUEST]: PUT [eventID]. In the actual code: 
+			// 1. the test stuff above will be removed, but we somehow still need to reference the object [testUser].
+			//		NOTE: may end up reconstructing the whole thing with a return or just create a global-scale variabe (i.e. where the test
+			//		information currently sits.)
+			//		Since we're only pushing, a global variable would probably be easy.
+			}
 			$('#eventCreateModal').removeClass("show");
 			$('#errorBox').removeClass("show")
-			console.log(createdEvents);
 			renderEvents();
-			}
 		}
-	
+
 	$('#cancelButton').on("click", emptyCart);
 	function emptyCart(event) {
 		event.preventDefault();
