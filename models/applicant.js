58ae76bbce89c6639b5dab867b690fba41f5fbe2
Josh McCormick <josh.mccormick81@gmail.com>
2018-11-14 10:32:50 -0500

    changed applicants model and seed data

--- models/applicant.js
+++ models/applicant.js
@@ -1,22 +1,15 @@
-module.exports = function(connection, Sequelize) {
+module.exports = function (connection, Sequelize) {
     const Applicant = connection.define('Applicant', {
-      offer:{
-          type:Sequelize.DECIMAL(6,2),
-          allowNull:false
-      },
-        bid_win:{
-          type:Sequelize.BOOLEAN
-      },
-      sales_pitch:{
-          type:Sequelize.STRING,
-          allowNull:true,
-          len:[0,250]
-      }
+
+        bid_win: {
+            type: Sequelize.BOOLEAN,
+            defaultValue:false
+        }
     });
-    Applicant.associate=function(models){
-        Applicant.belongsToMany(models.Artist,{through:'Artist_Event'});
-        Applicant.belongsToMany(models.Event,{through:'Artist_Event'});;
+    Applicant.associate = function (models) {
+        Applicant.belongsToMany(models.Artist, { through: 'Artist_Event' });
+        Applicant.belongsToMany(models.Event, { through: 'Artist_Event' });;
     }
-  
+
     return Applicant;
-  }
+}
