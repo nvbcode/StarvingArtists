0a9335488dd071fb3454689034f60ac9c9e2473e
unknown <bhandari.neel@outlook.com>
2018-11-14 14:27:48 -0500

    added multiple ajax calls

--- routes/api-applicant.js
+++ routes/api-applicant.js
@@ -1,50 +1,49 @@
 // Requiring our models
 const db = require('../models');
 
-module.exports = function(app){
+
+module.exports = function (app) {
 
     //Get all appications based on ArtistId
-    app.get("/api/applicants/:id", function(req, res){
+    app.get("/api/applicants/:id", function (req, res) {
 
         db.Applicant.findAll({ where: { EventId: req.params.id } })
             .then(function (applicant) {
                 res.json(applicant);
-                
+
             }).catch(function (error) {
                 res.json({ error: error });
             });
     });
 
-    app.post("/api/applicants", function(req, res){
+    app.post("/api/applicants", function (req, res) {
 
         const applicant = req.body;
-        
-        db.Applicant.create(applicant).then(function(row){
+
+        db.Applicant.create(applicant).then(function (row) {
 
             res.json({
                 Applicant_ID: row.dataValues.id,
                 Status: "Processed"
             })
 
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error);
-            res.json({Error: error})
+            res.json({ Error: error })
         });
 
     });
 
-    app.put("/api/applicants/:id", function(req, res) {
+    app.put("/api/applicants/:id", function (req, res) {
 
         const updatedApplicant = {
             id: req.params.id,
-            offer: req.body.offer,
             bid_win: req.body.bid_win,
-            sales_pitch: req.body.sales_pitch,
             ArtistId: req.body.ArtistId,
             EventId: req.body.EventId
         }
 
-        db.Applicant.update(updatedApplicant, {
+        db.Applicant.update(req.body, {
             where: {
                 id: req.params.id
             }
--- routes/api-events.js
+++ routes/api-events.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const Sequelize=require('sequelize');
-const sequelize = new Sequelize('starvingartist_db', 'root', 'georgia18',{'dialect':'mysql'});
+const sequelize = new Sequelize('starvingartist_db', 'root', 'openme',{'dialect':'mysql'});
 
 module.exports = function (app) {
 
@@ -49,30 +49,12 @@ module.exports = function (app) {
     //Update the event's data
     app.put("/api/events/:id", function (req, res) {
 
-            //Updated customer data
-            const updateEvent = {
-                id: req.params.id,
-                event_type: req.body.event_type,
-                street_address: req.body.street_address,
-                city: req.body.city,
-                state: req.body.state,
-                venue_name: req.body.venue_name,
-                budget: req.body.budget,
-                additional_info: req.body.additional_info,
-                has_booking: req.body.has_booking,
-                CustomerId: req.body.CustomerId
-            }
+
 
             //Insert the updated data into the customer's table
-            db.Event.update(updateEvent,
-
-                {
-                    where: {
-                        id: req.params.id
-                    }
-                }).then(function (dbPut) {
+            sequelize.query(`update events set has_booking=true where id='${req.params.id}'`).then(function (dbPut) {
                     res.json({
-                        user_name: `${req.params.id} ${req.body.venue_name}`,
+                        user_name: `${req.params.id}`,
                         Status: "Updated"
                     });
                 }).catch(function (error) {
